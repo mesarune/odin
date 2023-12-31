2071191fc203861fca52d96b97e54aa847b051f2
mesarune <yuito.taniguchi@gmail.com>
2023-12-31 12:59:58 +0900

    Finished foundations part1

---
+++ javascript-basics/fundamentals-part1/variable-tutorial.js
@@ -0,0 +1,11 @@
+let name;
+let admin;
+name = "john";
+admin = name;
+console.log(admin);
+let ourPlanetName = "Earth";
+let currentUser;
+
+const birthday = '18.04.1982';
+
+const age = someCode(birthday);

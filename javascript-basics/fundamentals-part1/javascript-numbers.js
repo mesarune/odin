e2c108e752131a18dcbb032ed00cc023593814ce
mesarune <yuito.taniguchi@gmail.com>
2024-01-02 12:15:37 +0900

    JavaScript numbers study

---
+++ javascript-basics/fundamentals-part1/javascript-numbers.js
@@ -0,0 +1,24 @@
+
+let y = 9999999999999999;
+let x = 0.2 + 0.1;
+x = (0.2 * 10 + 0.1 * 10) / 10;
+
+let myNumber = 2;
+// Execute until Infinity
+while (myNumber != Infinity) {
+  myNumber = myNumber * myNumber;
+}
+
+x =  0 * Infinity;
+y = -2 / Infinity;
+
+x = 0xFF;
+
+let x = new Number(500);
+let y = new Number(500);
+
+console.log(y==x);
+
+let n = Number.parseFloat("40です");
+
+console.log(n);

6540f691b35a8cc1a0c311baa4e6ff7aba9b886a
mesarune <yuito.taniguchi@gmail.com>
2024-01-02 10:55:36 +0900

    JavaScript arithmetic operators

---
+++ javascript-basics/fundamentals-part1/w3schools-lesson.js
@@ -0,0 +1,19 @@
+"use strict";
+
+let a = 100;
+let b = 50;
+
+let x = 100 + 50;
+x = a + b;
+x = (100 + 50) * a;
+x = a - b;
+x = a * b;
+x = a / b;
+x = a % b;
+a++;
+let z = a;
+z = a ** 2;
+
+
+// Log to console
+console.log(z);

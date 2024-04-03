0d4a8f8fa9bb6cc38d7e56526d8d9c35dcdae1e7
mesarune <yuito.taniguchi@gmail.com>
2024-04-03 23:22:37 +0900

    Etch a sketch

---
+++ javascript-basics/etch-a-sketch/01-etch-a-sketch.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const container = document.querySelector(".container");
+const div = document.createElement("div");
+div.setAttribute("class", "grid");
+
+container.appendChild(div);

e220879c8e8777b65721af44933957bd46e73e3c
mesarune <yuito.taniguchi@gmail.com>
2024-04-06 10:30:38 +0900

    Updated

--- javascript-basics/etch-a-sketch/03-etch-a-sketch.js
+++ javascript-basics/etch-a-sketch/03-etch-a-sketch.js
@@ -1,18 +1,29 @@
 "use strict";
 
 const container = document.querySelector(".container");
+const resetBtn = document.querySelector("#resetBtn");
+
+createGrid();
+
+resetBtn.addEventListener("click", () => {
+  container.innerHTML = "";
+  createGrid();
+})
 
 function createGrid() {
   for (let i = 0; i < 16; i++) {
     const row = document.createElement("div");
+    row.classList.add("row");
     container.appendChild(row);
     
     for (let j = 0; j < 16; j++) {
       const col = document.createElement("div");
       col.classList.add("grid");
       row.appendChild(col);
+      
+      col.addEventListener("mouseover", function() {
+        col.style.backgroundColor = "black";
+      });
     }
   }
 }
-
-createGrid();

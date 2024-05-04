31d88753b9008897f2b870154557bf8c8467dfe3
mesarune <yuito.taniguchi@gmail.com>
2024-05-04 22:54:42 +0900

    Little homework

--- javascript-basics/homework/fibonacci.js
+++ javascript-basics/homework/fibonacci.js
@@ -1,11 +1,21 @@
 "use strict";
 
+// const fibonacci = function(n) {
+//   let array = [1, 1];
+//   for (let i = 2; i < n; i++) {
+//     array.push(array.at(-2) + array.at(-1));
+//   }
+//   return array.at(-1);
+// };
+
 const fibonacci = function(n) {
-  let array = [1, 1];
+  let num1 = 1;
+  let num2 = 1;
   for (let i = 2; i < n; i++) {
-    array.push(array.at(-2) + array.at(-1));
+    num2 = num1 + num2;
+    num1 = num2 - num1;
   }
-  return array.at(-1);
+  return num2;
 };
 
-console.log(fibonacci(1));
+console.log(fibonacci(7));

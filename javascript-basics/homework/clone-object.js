4b65be63680dd85597dce1e1cbd91a1cf4563820
mesarune <yuito.taniguchi@gmail.com>
2024-05-20 22:54:57 +0900

    Update

--- javascript-basics/homework/clone-object.js
+++ javascript-basics/homework/clone-object.js
@@ -1,15 +1,26 @@
 "use strict";
 
 let list = {
-  value: 5,
-  tail: {
-    value: 3,
-    tail: {
-      value: 7
-    }
-  }
+  value: 5,
+  tail: {
+    value: 3,
+    tail: {
+      value: 7
+    }
+  }
 }
 
+// Existing function
 let newList = structuredClone(list);
 
-console.log(newList);
+// JSON
+let newObj = JSON.parse(JSON.stringify(list));
+
+// Recursive
+function deepCopy(obj) {
+  return Object.keys(obj).reduce((v, d) => Object.assign(v, {
+    [d]: (obj[d].constructor === Object) ? deepCopy(obj[d]) : obj[d]
+  }), {});
+}
+
+console.log(newObj);

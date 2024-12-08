08866094c0707e73cbe9d6e9cba65f15e2e6664f
mesarune <yuito.taniguchi@gmail.com>
2024-12-08 20:50:13 +0900

    Caesar cipher improved

--- ruby-basics/caesar_cipher/02_caesar_cipher.rb
+++ ruby-basics/caesar_cipher/02_caesar_cipher.rb
@@ -16,19 +16,19 @@ def caesar_cipher(string, number, table)
   end
   puts result # This return "nil"
 end
-
 => :caesar_cipher
-    irb(main):078> caesar_cipher(caesar_cipher("Caesar cipher", 6, ("A".."Z").to_a), 6, ("a".."z").to_a)
-    Iaesar cipher
-    (irb):62:in `caesar_cipher': undefined method `split' for nil (NoMethodError)
-    
-      string_array = string.split("")
-                           ^^^^^^
-        from (irb):78:in `<main>'
-        from <internal:kernel>:187:in `loop'
-        from /Users/mesa_rune/.rbenv/versions/3.3.4/lib/ruby/gems/3.3.0/gems/irb-1.13.1/exe/irb:9:in `<top (required)>'
-        from /Users/mesa_rune/.rbenv/versions/3.3.4/bin/irb:25:in `load'
-        from /Users/mesa_rune/.rbenv/versions/3.3.4/bin/irb:25:in `<main>'
+
+caesar_cipher(caesar_cipher("Caesar cipher", 6, ("A".."Z").to_a), 6, ("a".."z").to_a)
+   Iaesar cipher
+   (irb):62:in `caesar_cipher': undefined method `split' for nil (NoMethodError)
+   
+     string_array = string.split("")
+                          ^^^^^^
+       from (irb):78:in `<main>'
+       from <internal:kernel>:187:in `loop'
+       from /Users/mesa_rune/.rbenv/versions/3.3.4/lib/ruby/gems/3.3.0/gems/irb-1.13.1/exe/irb:9:in `<top (required)>'
+       from /Users/mesa_rune/.rbenv/versions/3.3.4/bin/irb:25:in `load'
+       from /Users/mesa_rune/.rbenv/versions/3.3.4/bin/irb:25:in `<main>'
 
 def caesar_cipher(string, number, table)
   result = ""

d720409227d3e53899344d55a351e9492dda7e4a
mesarune <yuito.taniguchi@gmail.com>
2025-03-09 11:00:14 +0900

    Tic tac toe

---
+++ ruby-basics/tic_tac_toe/board.rb
@@ -0,0 +1,34 @@
+class Board
+    def initialize
+        board = Array.new(3) { Array.new(3, " ") }
+    end
+    
+    def display_board
+        puts board.map { |row| row.join("|") }.join("\n-+-+-\n")
+    end
+
+    def check_result(player)
+        3.times do |i|
+            if board[i][0] == player && board[i][1] == player && board[1][2] == player
+                return true
+            end
+        end
+
+        3.times do |i|
+            if board[0][i] == player && board[1][i] == player && board[2][i] == player
+                return true
+            end
+        end
+
+        if board[0][0] == player && board[1][1] == player && board[2][2] == player
+            return true
+        end
+
+        if board[0][2] == player && board[1][1] == player && board[2][0] == player
+            return true
+        end
+
+        return false
+    end
+
+end

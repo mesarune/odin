dc165b863aa354d9e8ce7c7bcbf53ca98caf1b3e
mesarune <9974910+mesarune@users.noreply.github.com>
2025-06-08 11:41:13 +0900

    Update player.rb

--- ruby-basics/mastermind/player.rb
+++ ruby-basics/mastermind/player.rb
@@ -1,13 +1,13 @@
 class Player
 
-    @valid_input = ["r", "g", "y", "p", "m", "c"]
+    @@valid_input = ["r", "g", "y", "p", "m", "c"]
 
     def get_input
         loop do
             puts "入力してください"
             input = gets.chomp.split
             
-            if !input.all? { |s| valid_input.include?(s) }
+            if !input.all? { |s| @@valid_input.include?(s) }
              puts "無効な入力です"
             elsif !input.length == 4
              puts "無効な入力です"
